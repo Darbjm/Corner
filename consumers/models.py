5998017067a52710a912c29af84103ecb252da39
James Darby <jamesdarby1@hotmail.com>
2020-11-26 11:00:30 +0000

    Begin TDD

--- consumers/models.py
+++ consumers/models.py
@@ -7,7 +7,7 @@ class Consumer(models.Model):
     username = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    areacode = models.CharField(max_length=15)
+    # areacode = models.CharField(max_length=15)
 
-    def __str__(self):
-        return f'{self.username} - {self.areacode}'
+    # def __str__(self):
+    #     return f'{self.username} - {self.areacode}'

25f2911337326e5a28efa27f7531dfd66034364b
James Darby <jamesdarby1@hotmail.com>
2020-12-10 14:31:57 +0000

    setup foods model

--- foods/models.py
+++ foods/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Consumer(Models.models):
+class Food(Models.models):
 
     api_id = models.CharField(max_length=50)
     title = models.CharField(max_length=500)

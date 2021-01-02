0ffb1f1b36cf84af156a2582f8550f0b4d222235
James Darby <56593075+Darbjm@users.noreply.github.com>
2021-01-02 23:21:50 +0000

    fixed models

--- consumers/models.py
+++ consumers/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from foods.models import Food
 # Create your models here.
 
 
 class Consumer(AbstractUser):
 
-    password = models.CharField(max_length=50)
-    areacode = models.CharField(max_length=15)
+    Areacode = models.CharField(max_length=15)
+    Likes = models.ManyToManyField(Food, related_name='Consumer_Likes')
+    Dislikes = models.ManyToManyField(Food, related_name='Consumers_Dislikes')
 
     def __str__(self):
-        return f'{self.username} - {self.areacode}'
+        return f'{self.username} - {self.Areacode}'
--- foods/models.py
+++ foods/models.py
@@ -1,40 +1,49 @@
 # pylint: disable=no-member
 from django.db import models
 
-class Food(models.Models):
-    api_id = models.CharField(max_length=50)
-    title = models.CharField(max_length=500)
-    breadcrumbs = models.CharField(max_length=500)
-    badges = models.CharField(max_length=500)
-    importantBadges = models.CharField(max_length=500)
-    ingredientCount = models.CharField(max_length=10)
-    generatedText = models.CharField(max_length=500)
-    ingredientList = models.CharField(max_length=500)
-    likes = models.CharField(max_length=50)
-    aisle = models.CharField(max_length=500)
-    price = models.CharField(max_length=500)
 
-class ingredients(models.Models):
-    Food = models.ForeignKey(Food, related_name='Food', on_delete=models.CASCADE)
-    description = models.CharField(max_length=1000)
-    name = models.CharField(max_length=1000)
-    safety_level = models.CharField(max_length=1000)
+class Ingredients(models.Model):
+    Description = models.CharField(max_length=1000)
+    Name = models.CharField(max_length=1000)
+    Safety_Level = models.CharField(max_length=1000)
 
-class nutrients(models.Models):
-    Food = models.ForeignKey(Food, related_name='Food', on_delete=models.CASCADE)
-    title = models.CharField(max_length=1000)
-    amount = models.CharField(max_length=1000)
-    unit = models.CharField(max_length=1000)
-    percentOfDailyNeeds = models.CharField(max_length=1000)
 
-class caloricBreakdown(models.Models):
-    Food = models.ForeignKey(Food, related_name='Food', on_delete=models.CASCADE)
-    percentProtein = models.CharField(max_length=1000)
-    percentFat = models.CharField(max_length=1000)
-    percentCarbs = models.CharField(max_length=1000)
+class Nutrients(models.Model):
+    Title = models.CharField(max_length=1000)
+    Amount = models.CharField(max_length=1000)
+    Unit = models.CharField(max_length=1000)
+    Percent_Of_Daily_Needs = models.CharField(max_length=1000)
 
-class servings(models.Models):
-    Food = models.ForeignKey(Food, related_name='Food', on_delete=models.CASCADE)
-    number = models.CharField(max_length=1000)
-    size = models.CharField(max_length=1000)
-    unit = models.CharField(max_length=1000)
+
+class CaloricBreakdown(models.Model):
+    Percent_Protein = models.CharField(max_length=1000)
+    Percent_Fat = models.CharField(max_length=1000)
+    Percent_Carbs = models.CharField(max_length=1000)
+
+
+class Servings(models.Model):
+    Number = models.CharField(max_length=1000)
+    Size = models.CharField(max_length=1000)
+    Unit = models.CharField(max_length=1000)
+
+
+class Food(models.Model):
+    Api_Id = models.CharField(max_length=50)
+    Title = models.CharField(max_length=500)
+    Breadcrumbs = models.CharField(max_length=500)
+    Badges = models.CharField(max_length=500)
+    Important_Badges = models.CharField(max_length=500)
+    Ingredient_Count = models.CharField(max_length=10)
+    Generated_Text = models.CharField(max_length=500)
+    Ingredient_List = models.CharField(max_length=500)
+    Likes = models.CharField(max_length=50)
+    Aisle = models.CharField(max_length=500)
+    Price = models.CharField(max_length=500)
+    Ingredients = models.ForeignKey(
+        Ingredients, related_name='Ingredients', null=True, on_delete=models.CASCADE)
+    Nutrients = models.ForeignKey(
+        Nutrients, related_name='Nutrients', null=True, on_delete=models.CASCADE)
+    CaloricBreakdown = models.ForeignKey(
+        CaloricBreakdown, related_name='CaloricBreakdown', null=True, on_delete=models.CASCADE)
+    Servings = models.ForeignKey(
+        Servings, related_name='Servings', null=True, on_delete=models.CASCADE)

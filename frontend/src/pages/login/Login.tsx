872ab3d62b78ed122f3459e0ccd6fe71e82c9d8c
James Darby <56593075+Darbjm@users.noreply.github.com>
2021-01-03 22:57:11 +0000

    login and register pages built

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,8 +1,7 @@
 import React, { useState, FormEvent } from 'react';
-import SimpleCard from '../../components/card';
-import Typography from '../../components/typography';
-import StandardTextField from '../../components/textField';
-import SimpleButton from '../../components/button';
+import Form from '../../components/form'
+import Card from '../../components/card'
+import TextField from '../../components/textField'
 
 const Login = () => {
   const [errorMessage, setErrorMessage] = useState('');
@@ -24,38 +23,12 @@ const Login = () => {
   };
 
   return (
-    <SimpleCard>
-      <Typography variant="h1" color="secondary">
-        Login
-      </Typography>
-      <form onSubmit={(e: FormEvent<HTMLFormElement>) => handleSubmit(e)}>
-        <div className="field">
-          <div className="control">
-            <StandardTextField
-              error={!!errorMessage}
-              help={errorMessage}
-              onChange={handleChange}
-              elName="Username"
-            />
-          </div>
-        </div>
-        <div className="field">
-          <div className="control">
-            <StandardTextField
-              error={!!errorMessage}
-              help={errorMessage}
-              onChange={handleChange}
-              elName="Password"
-              type="password"
-            />
-          </div>
-        </div>
-        <br />
-        <SimpleButton buttonSize="large" isFullWidth={true} color="secondary">
-          Login
-        </SimpleButton>
-      </form>
-    </SimpleCard>
+    <Card>
+      <Form title='Login' buttonName='Login' buttonColor='primary' handleSubmit={() => console.log('working')}>
+        <TextField elName='Username' color='primary' onChange={() => console.log('working')} />
+        <TextField elName='Password' color='primary' onChange={() => console.log('working')} />
+      </Form>
+    </Card>
   );
 };
 
--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Card from '../../components/card'
+import Form from '../../components/form'
+import TextField from '../../components/textField'
 
 const Register = () => {
 
   return (
-    <h1>Register</h1>
+    <Card>
+      <Form title='Register' buttonName='Submit' buttonColor='primary' handleSubmit={() => console.log('working')}>
+        <TextField elName='Username' color='primary' onChange={() => console.log('working')} />
+        <TextField elName='Area code' color='primary' onChange={() => console.log('working')} />
+        <TextField elName='Password' color='primary' onChange={() => console.log('working')} />
+        <TextField elName='Password confirmation' color='primary' onChange={() => console.log('working')} />
+      </Form>
+    </Card>
   )
 }
 

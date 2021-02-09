bc8a9689e46ce7a9f725a6d239a80fc8602d10d3
James Darby <56593075+Darbjm@users.noreply.github.com>
2021-02-09 09:54:48 +0300

    tdd complete

--- consumers/tests.py
+++ consumers/tests.py
@@ -1,231 +1,231 @@
-# import base64
-# import jwt
-# from django.conf import settings
-# from django.urls import reverse
-# from rest_framework.test import APITestCase
-# from django.contrib.auth.models import AnonymousUser
+import base64
+import jwt
+from django.conf import settings
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import AnonymousUser
 
-# user_data = {
-#     'username': 'test',
-#     'area_code': 'SE1',
-#     'password': 'MyDiffPass96',
-#     'password_confirmation': 'MyDiffPass96',
-# }
+user_data = {
+    'username': 'test',
+    'area_code': 'SE1',
+    'password': 'MyDiffPass96',
+    'password_confirmation': 'MyDiffPass96',
+}
 
-# REQUIRED_FIELD = 'This field is required.'
+REQUIRED_FIELD = 'This field is required.'
 
-# BEARER = 'Bearer '
-
-
-# class RegisterTest(APITestCase):
-#     def setUp(self):
-#         self.register_url = reverse('register')
-#         self.login_url = reverse('login')
-#         self.simple_password = {
-#             'username': 'test',
-#             'area_code': 'SE1',
-#             'password': 'Password1',
-#             'password_confirmation': 'Password1',
-#         }
-#         self.password_does_not_match = {
-#             'username': 'test',
-#             'area_code': 'SE1',
-#             'password': 'Password1',
-#             'password_confirmation': 'Pass',
-#         }
-#         self.no_username = {
-#             'username': '',
-#             'area_code': 'SE1',
-#             'password': 'Password1',
-#             'password_confirmation': 'Password1',
-#         }
-#         self.no_area_code = {
-#             'username': 'test',
-#             'area_code': '',
-#             'password': 'Password1',
-#             'password_confirmation': 'Password1',
-#         }
-
-#     def test_can_view_page_correctly(self):
-#         response = self.client.get(self.register_url)
-#         self.assertEqual(response.status_code, 200)
-
-#     def test_can_register_user(self):
-#         response = self.client.post(self.register_url, user_data)
-#         self.assertEqual(response.status_code, 201)
-#         self.assertEqual(response.data, {'message': 'Registration Succesful'})
-
-#     def test_will_check_password(self):
-#         response = self.client.post(self.register_url, self.simple_password)
-#         self.assertEqual(response.status_code, 422)
-#         self.assertEqual(
-#             str(response.data['password'][0]), 'Password must be 8 characters long, Difficult to guess, and contain a letter')
-
-#     def test_will_check_password_matches(self):
-#         response = self.client.post(
-#             self.register_url, self.password_does_not_match)
-#         self.assertEqual(response.status_code, 422)
-#         self.assertEqual(
-#             str(response.data['password_confirmation'][0]), 'Does Not Match')
-
-#     def test_will_check_for_username(self):
-#         response = self.client.post(
-#             self.register_url, self.no_username)
-#         self.assertEqual(response.status_code, 422)
-#         self.assertEqual(
-#             str(response.data['username'][0]), 'This field may not be blank.')
-
-#     def test_will_check_for_area_code(self):
-#         response = self.client.post(
-#             self.register_url, self.no_area_code)
-#         self.assertEqual(response.status_code, 422)
-#         self.assertEqual(
-#             str(response.data['area_code'][0]), 'This field may not be blank.')
-
-# # Unsure whether these are intergration tests - need research
+BEARER = 'Bearer '
 
 
-# class LoginTest(APITestCase):
+class RegisterTest(APITestCase):
+    def setUp(self):
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.simple_password = {
+            'username': 'test',
+            'area_code': 'SE1',
+            'password': 'Password1',
+            'password_confirmation': 'Password1',
+        }
+        self.password_does_not_match = {
+            'username': 'test',
+            'area_code': 'SE1',
+            'password': 'Password1',
+            'password_confirmation': 'Pass',
+        }
+        self.no_username = {
+            'username': '',
+            'area_code': 'SE1',
+            'password': 'Password1',
+            'password_confirmation': 'Password1',
+        }
+        self.no_area_code = {
+            'username': 'test',
+            'area_code': '',
+            'password': 'Password1',
+            'password_confirmation': 'Password1',
+        }
 
-#     def setUp(self):
-#         self.client.post(reverse('register'), user_data)
-#         self.login_url = reverse('login')
-#         self.simple_password = {
-#             'username': 'test',
-#             'area_code': 'SE1',
-#             'password': 'Password1',
-#             'password_confirmation': 'Password1',
-#         }
-#         self.unregistered_user = {
-#             'username': 'unreg',
-#             'area_code': 'SE1',
-#             'password': 'MyDiffUnreg184',
-#             'password_confirmation': 'MyDiffUnreg184',
-#         }
+    def test_can_view_page_correctly(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
 
-#     def test_can_view_page_correctly(self):
-#         response = self.client.get(self.login_url)
-#         self.assertEqual(response.status_code, 200)
+    def test_can_register_user(self):
+        response = self.client.post(self.register_url, user_data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data, {'message': 'Registration Succesful'})
 
-#     def test_can_login(self):
-#         response = self.client.post(self.login_url, user_data)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(type(response.data['token']), str)
-#         username = user_data['username']
-#         self.assertEqual(response.data['message'], f'Welcome back {username}')
+    def test_will_check_password(self):
+        response = self.client.post(self.register_url, self.simple_password)
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            str(response.data['password'][0]), 'Password must be 8 characters long, Difficult to guess, and contain a letter')
 
-#     def test_cannot_login_before_registration(self):
-#         response = self.client.post(self.login_url, self.unregistered_user)
-#         self.assertEqual(response.status_code, 403)
-#         self.assertEqual(response.data['message'], 'Invalid Credentials')
+    def test_will_check_password_matches(self):
+        response = self.client.post(
+            self.register_url, self.password_does_not_match)
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            str(response.data['password_confirmation'][0]), 'Does Not Match')
 
-#     def test_cannot_login_with_wrong_password(self):
-#         response = self.client.post(self.login_url, self.simple_password)
-#         self.assertEqual(response.status_code, 403)
-#         self.assertEqual(response.data['message'], 'Invalid Credentials')
+    def test_will_check_for_username(self):
+        response = self.client.post(
+            self.register_url, self.no_username)
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            str(response.data['username'][0]), 'This field may not be blank.')
 
+    def test_will_check_for_area_code(self):
+        response = self.client.post(
+            self.register_url, self.no_area_code)
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            str(response.data['area_code'][0]), 'This field may not be blank.')
 
-# class UserViewTest(APITestCase):
-#     def setUp(self):
-#         self.client.post(reverse('register'), user_data)
-#         response = self.client.post(reverse('login'), user_data)
-#         self.token = response.data['token']
-#         user_token = jwt.decode(
-#             self.token, settings.SECRET_KEY, algorithms=['HS256'])
-#         self.user_id = user_token['sub']
-
-#     def test_logged_in_user_can_view_profile(self):
-#         request = self.client.get(
-#             reverse('profile', kwargs={'pk': self.user_id}), **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#         self.assertEqual(request.status_code, 200)
-#         self.assertEqual(request.data['username'], user_data['username'])
-#         self.assertEqual(request.data['area_code'], user_data['area_code'])
-#         self.assertEqual(request.data['id'], self.user_id)
-
-#     def test_user_without_token_cannot_access_profile(self):
-#         request = self.client.get(
-#             reverse('profile', kwargs={'pk': self.user_id}))
-#         self.assertEqual(request.status_code, 401)
-#         self.assertEqual(
-#             request.data['detail'], 'Authentication credentials were not provided.')
-
-#     def test_token_needs_bearer(self):
-#         request = self.client.get(
-#             reverse('profile', kwargs={'pk': self.user_id}), **{'HTTP_AUTHORIZATION': self.token})
-#         self.assertEqual(request.status_code, 403)
-#         self.assertEqual(
-#             request.data['message'], 'Invalid Authorization Header')
-
-#     def test_token_needs_to_be_authentic(self):
-#         request = self.client.get(
-#             reverse('profile', kwargs={'pk': self.user_id}), **{'HTTP_AUTHORIZATION': 'evnjksdnfknaiuerhfa'})
-#         self.assertEqual(request.status_code, 403)
-#         self.assertEqual(
-#             request.data['message'], 'Invalid Authorization Header')
+# Unsure whether these are intergration tests - need research
 
 
-# class UserEditTest(APITestCase):
-#     def setUp(self):
-#         self.client.post(reverse('register'), user_data)
-#         response = self.client.post(reverse('login'), user_data)
-#         self.token = response.data['token']
-#         user_token = jwt.decode(
-#             self.token, settings.SECRET_KEY, algorithms=['HS256'])
-#         self.user_id = user_token['sub']
+class LoginTest(APITestCase):
+
+    def setUp(self):
+        self.client.post(reverse('register'), user_data)
+        self.login_url = reverse('login')
+        self.simple_password = {
+            'username': 'test',
+            'area_code': 'SE1',
+            'password': 'Password1',
+            'password_confirmation': 'Password1',
+        }
+        self.unregistered_user = {
+            'username': 'unreg',
+            'area_code': 'SE1',
+            'password': 'MyDiffUnreg184',
+            'password_confirmation': 'MyDiffUnreg184',
+        }
+
+    def test_can_view_page_correctly(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_login(self):
+        response = self.client.post(self.login_url, user_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(type(response.data['token']), str)
+        username = user_data['username']
+        self.assertEqual(response.data['message'], f'Welcome back {username}')
+
+    def test_cannot_login_before_registration(self):
+        response = self.client.post(self.login_url, self.unregistered_user)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data['message'], 'Invalid Credentials')
+
+    def test_cannot_login_with_wrong_password(self):
+        response = self.client.post(self.login_url, self.simple_password)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data['message'], 'Invalid Credentials')
 
 
-# def test_logged_in_user_can_edit_profile(self):
-#     new_user = {
-#         'username': 'test2',
-#         'area_code': 'NN1'
-#     }
-#     request = self.client.put(
-#         reverse('edit', kwargs={'pk': self.user_id}), new_user, **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#     self.assertEqual(request.status_code, 202)
-#     self.assertEqual(request.data['username'], new_user['username'])
-#     self.assertEqual(request.data['area_code'], new_user['area_code'])
-#     self.assertEqual(request.data['id'], self.user_id)
+class UserViewTest(APITestCase):
+    def setUp(self):
+        self.client.post(reverse('register'), user_data)
+        response = self.client.post(reverse('login'), user_data)
+        self.token = response.data['token']
+        user_token = jwt.decode(
+            self.token, settings.SECRET_KEY, algorithms=['HS256'])
+        self.user_id = user_token['sub']
+
+    def test_logged_in_user_can_view_profile(self):
+        request = self.client.get(
+            reverse('profile', kwargs={'pk': self.user_id}), **{'HTTP_AUTHORIZATION': BEARER + self.token})
+        self.assertEqual(request.status_code, 200)
+        self.assertEqual(request.data['username'], user_data['username'])
+        self.assertEqual(request.data['area_code'], user_data['area_code'])
+        self.assertEqual(request.data['id'], self.user_id)
+
+    def test_user_without_token_cannot_access_profile(self):
+        request = self.client.get(
+            reverse('profile', kwargs={'pk': self.user_id}))
+        self.assertEqual(request.status_code, 401)
+        self.assertEqual(
+            request.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_token_needs_bearer(self):
+        request = self.client.get(
+            reverse('profile', kwargs={'pk': self.user_id}), **{'HTTP_AUTHORIZATION': self.token})
+        self.assertEqual(request.status_code, 403)
+        self.assertEqual(
+            request.data['message'], 'Invalid Authorization Header')
+
+    def test_token_needs_to_be_authentic(self):
+        request = self.client.get(
+            reverse('profile', kwargs={'pk': self.user_id}), **{'HTTP_AUTHORIZATION': 'evnjksdnfknaiuerhfa'})
+        self.assertEqual(request.status_code, 403)
+        self.assertEqual(
+            request.data['message'], 'Invalid Authorization Header')
 
 
-# def test_user_without_token_cannot_edit_profile(self):
-#     new_user = {
-#         'username': 'test2',
-#         'area_code': 'NN1'
-#     }
-#     request = self.client.put(
-#         reverse('edit', kwargs={'pk': self.user_id}), new_user)
-#     self.assertEqual(request.status_code, 401)
-#     self.assertEqual(
-#         request.data['detail'], 'Authentication credentials were not provided.')
+class UserEditTest(APITestCase):
+    def setUp(self):
+        self.client.post(reverse('register'), user_data)
+        response = self.client.post(reverse('login'), user_data)
+        self.token = response.data['token']
+        user_token = jwt.decode(
+            self.token, settings.SECRET_KEY, algorithms=['HS256'])
+        self.user_id = user_token['sub']
 
 
-# def test_user_must_send_information(self):
-#     new_user = {
-#     }
-#     request = self.client.put(
-#         reverse('edit', kwargs={'pk': self.user_id}), new_user, **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#     self.assertEqual(request.status_code, 422)
-#     self.assertEqual(
-#         request.data['username'][0], REQUIRED_FIELD)
-#     self.assertEqual(
-#         request.data['area_code'][0], REQUIRED_FIELD)
+def test_logged_in_user_can_edit_profile(self):
+    new_user = {
+        'username': 'test2',
+        'area_code': 'NN1'
+    }
+    request = self.client.put(
+        reverse('edit', kwargs={'pk': self.user_id}), new_user, **{'HTTP_AUTHORIZATION': BEARER + self.token})
+    self.assertEqual(request.status_code, 202)
+    self.assertEqual(request.data['username'], new_user['username'])
+    self.assertEqual(request.data['area_code'], new_user['area_code'])
+    self.assertEqual(request.data['id'], self.user_id)
 
 
-# class UserDeleteTest(APITestCase):
-#     def setUp(self):
-#         self.client.post(reverse('register'), user_data)
-#         response = self.client.post(reverse('login'), user_data)
-#         self.token = response.data['token']
-#         user_token = jwt.decode(
-#             self.token, settings.SECRET_KEY, algorithms=['HS256'])
-#         self.user_id = user_token['sub']
+def test_user_without_token_cannot_edit_profile(self):
+    new_user = {
+        'username': 'test2',
+        'area_code': 'NN1'
+    }
+    request = self.client.put(
+        reverse('edit', kwargs={'pk': self.user_id}), new_user)
+    self.assertEqual(request.status_code, 401)
+    self.assertEqual(
+        request.data['detail'], 'Authentication credentials were not provided.')
 
-#     def test_logged_in_user_can_delete_profile(self):
-#         request = self.client.delete(
-#             reverse('edit', kwargs={'pk': self.user_id}), **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#         self.assertEqual(request.status_code, 204)
 
-#     def test_user_without_token_cannot_delete_profile(self):
-#         request = self.client.delete(
-#             reverse('edit', kwargs={'pk': self.user_id}))
-#         self.assertEqual(request.status_code, 401)
+def test_user_must_send_information(self):
+    new_user = {
+    }
+    request = self.client.put(
+        reverse('edit', kwargs={'pk': self.user_id}), new_user, **{'HTTP_AUTHORIZATION': BEARER + self.token})
+    self.assertEqual(request.status_code, 422)
+    self.assertEqual(
+        request.data['username'][0], REQUIRED_FIELD)
+    self.assertEqual(
+        request.data['area_code'][0], REQUIRED_FIELD)
+
+
+class UserDeleteTest(APITestCase):
+    def setUp(self):
+        self.client.post(reverse('register'), user_data)
+        response = self.client.post(reverse('login'), user_data)
+        self.token = response.data['token']
+        user_token = jwt.decode(
+            self.token, settings.SECRET_KEY, algorithms=['HS256'])
+        self.user_id = user_token['sub']
+
+    def test_logged_in_user_can_delete_profile(self):
+        request = self.client.delete(
+            reverse('edit', kwargs={'pk': self.user_id}), **{'HTTP_AUTHORIZATION': BEARER + self.token})
+        self.assertEqual(request.status_code, 204)
+
+    def test_user_without_token_cannot_delete_profile(self):
+        request = self.client.delete(
+            reverse('edit', kwargs={'pk': self.user_id}))
+        self.assertEqual(request.status_code, 401)
--- foods/tests.py
+++ foods/tests.py
@@ -61,99 +61,99 @@ REQUIRED_FIELD = 'This field is required.'
 BEARER = 'Bearer '
 
 
-# class AddFoodTest(APITestCase):
-#     def setUp(self):
-#         self.client.post(reverse('register'), user_data)
-#         self.login_url = reverse('login')
-#         self.add_url = reverse('addfood')
-#         response = self.client.post(reverse('login'), user_data)
-#         self.token = response.data['token']
-#         user_token = jwt.decode(
-#             self.token, settings.SECRET_KEY, algorithms=['HS256'])
-#         self.user_id = user_token['sub']
+class AddFoodTest(APITestCase):
+    def setUp(self):
+        self.client.post(reverse('register'), user_data)
+        self.login_url = reverse('login')
+        self.add_url = reverse('addfood')
+        response = self.client.post(reverse('login'), user_data)
+        self.token = response.data['token']
+        user_token = jwt.decode(
+            self.token, settings.SECRET_KEY, algorithms=['HS256'])
+        self.user_id = user_token['sub']
 
-#     def test_can_view_page_correctly(self):
-#         response = self.client.get(
-#             self.add_url, **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#         self.assertEqual(response.status_code, 200)
+    def test_can_view_page_correctly(self):
+        response = self.client.get(
+            self.add_url, **{'HTTP_AUTHORIZATION': BEARER + self.token})
+        self.assertEqual(response.status_code, 200)
 
-#     def test_can_add_food(self):
-#         response = self.client.post(
-#             self.add_url, food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#         self.assertEqual(response.status_code, 201)
-#         self.assertEqual(
-#             response.data, {'id': 1, 'name': 'Sathers Candy Corn 125g', 'image': 'cdn.shopify.com/s/files/1/0342/2388/2379/products/sathers-candy-corn-3-25oz-92g-800x800_750x.png?v=1588789406', 'price': '1.89', 'description': 'sweet', 'likes': [1], "dislikes": [], 'creator': 1})
+    def test_can_add_food(self):
+        response = self.client.post(
+            self.add_url, food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(
+            response.data, {'id': 1, 'name': 'Sathers Candy Corn 125g', 'image': 'cdn.shopify.com/s/files/1/0342/2388/2379/products/sathers-candy-corn-3-25oz-92g-800x800_750x.png?v=1588789406', 'price': '1.89', 'description': 'sweet', 'likes': [1], "dislikes": [], 'creator': 1})
 
-#     def test_will_check_food_has_name(self):
-#         response = self.client.post(
-#             self.add_url, bad_name_food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#         self.assertEqual(response.status_code, 422)
-#         self.assertEqual(
-#             response.data['name'][0], 'This field is required.')
+    def test_will_check_food_has_name(self):
+        response = self.client.post(
+            self.add_url, bad_name_food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            response.data['name'][0], 'This field is required.')
 
-#     def test_will_check_food_has_image(self):
-#         response = self.client.post(
-#             self.add_url, bad_name_food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#         self.assertEqual(response.status_code, 422)
-#         self.assertEqual(
-#             response.data['name'][0], 'This field is required.')
+    def test_will_check_food_has_image(self):
+        response = self.client.post(
+            self.add_url, bad_name_food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            response.data['name'][0], 'This field is required.')
 
-#     def test_will_not_create_duplicate_name(self):
-#         self.client.post(self.add_url, food_data, **
-#                          {'HTTP_AUTHORIZATION': BEARER + self.token})
-#         response = self.client.post(
-#             self.add_url, food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#         self.assertEqual(response.status_code, 422)
-#         self.assertEqual(
-#             response.data['name'][0], 'food with this name already exists.')
+    def test_will_not_create_duplicate_name(self):
+        self.client.post(self.add_url, food_data, **
+                         {'HTTP_AUTHORIZATION': BEARER + self.token})
+        response = self.client.post(
+            self.add_url, food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            response.data['name'][0], 'food with this name already exists.')
 
-#     def test_will_not_create_duplicate_image(self):
-#         self.client.post(self.add_url, food_data, **
-#                          {'HTTP_AUTHORIZATION': BEARER + self.token})
-#         response = self.client.post(
-#             self.add_url, different_name_food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
-#         self.assertEqual(response.status_code, 422)
-#         self.assertEqual(
-#             response.data['image'][0], 'food with this image already exists.')
-
-
-# class ViewAllFoodTest(APITestCase):
-#     def setUp(self):
-#         self.client.post(reverse('register'), user_data)
-#         self.login_url = reverse('login')
-#         self.all_url = reverse('allfood')
-#         self.add_url = reverse('addfood')
-#         response = self.client.post(reverse('login'), user_data)
-#         self.token = response.data['token']
-#         user_token = jwt.decode(
-#             self.token, settings.SECRET_KEY, algorithms=['HS256'])
-#         self.user_id = user_token['sub']
-#         self.client.post(self.add_url, food_data, **
-#                          {'HTTP_AUTHORIZATION': BEARER + self.token})
-
-#     def test_can_view_all_foods(self):
-#         response = self.client.get(self.all_url)
-#         self.assertEqual(response.status_code, 200)
-#         json_response = dict((response.data[0]))
-#         self.assertEqual(
-#             json_response, {
-#                 "id": 1,
-#                 "name": "Sathers Candy Corn 125g",
-#                 "image": "cdn.shopify.com/s/files/1/0342/2388/2379/products/sathers-candy-corn-3-25oz-92g-800x800_750x.png?v=1588789406",
-#                 "price": "1.89",
-#                 "description": "sweet",
-#                 "likes": [1],
-#                 "dislikes": [],
-#                 "creator": 1})
+    def test_will_not_create_duplicate_image(self):
+        self.client.post(self.add_url, food_data, **
+                         {'HTTP_AUTHORIZATION': BEARER + self.token})
+        response = self.client.post(
+            self.add_url, different_name_food_data, **{'HTTP_AUTHORIZATION': BEARER + self.token})
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            response.data['image'][0], 'food with this image already exists.')
 
 
-# class ScrapeSnacks(APITestCase):
-#     def setUp(self):
-#         self.snacks_url = reverse('snacks')
+class ViewAllFoodTest(APITestCase):
+    def setUp(self):
+        self.client.post(reverse('register'), user_data)
+        self.login_url = reverse('login')
+        self.all_url = reverse('allfood')
+        self.add_url = reverse('addfood')
+        response = self.client.post(reverse('login'), user_data)
+        self.token = response.data['token']
+        user_token = jwt.decode(
+            self.token, settings.SECRET_KEY, algorithms=['HS256'])
+        self.user_id = user_token['sub']
+        self.client.post(self.add_url, food_data, **
+                         {'HTTP_AUTHORIZATION': BEARER + self.token})
 
-#     def test_can_scape_web_for_snacks(self):
-#         response = self.client.get(self.snacks_url)
-#         self.assertEqual(response.data[0]['model'], 'foods.food')
+    def test_can_view_all_foods(self):
+        response = self.client.get(self.all_url)
+        self.assertEqual(response.status_code, 200)
+        json_response = dict((response.data[0]))
+        self.assertEqual(
+            json_response, {
+                "id": 1,
+                "name": "Sathers Candy Corn 125g",
+                "image": "cdn.shopify.com/s/files/1/0342/2388/2379/products/sathers-candy-corn-3-25oz-92g-800x800_750x.png?v=1588789406",
+                "price": "1.89",
+                "description": "sweet",
+                "likes": [1],
+                "dislikes": [],
+                "creator": 1})
+
+
+class ScrapeSnacks(APITestCase):
+    def setUp(self):
+        self.snacks_url = reverse('snacks')
+
+    def test_can_scape_web_for_snacks(self):
+        response = self.client.get(self.snacks_url)
+        self.assertEqual(response.data[0]['model'], 'foods.food')
 
 
 class EditFoodTest(APITestCase):

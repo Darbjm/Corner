166a729e6ac7502a3118ca7e17867b22d00f2b4a
James Darby <56593075+Darbjm@users.noreply.github.com>
2021-02-18 12:59:11 +0300

    fix map bug

---
+++ frontend/build/precache-manifest.15f30a9383bf8ecfc51f92b36ffa9b83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a77d4b2bbc8a138e7bd8d479c3cc29f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf929c15673833d15b3a",
+    "url": "/static/css/main.872938e4.chunk.css"
+  },
+  {
+    "revision": "cfd22c52642e5ba0897d",
+    "url": "/static/js/2.54b8525c.chunk.js"
+  },
+  {
+    "revision": "bd7b26c0c34ce8f026397d952561bcd4",
+    "url": "/static/js/2.54b8525c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf929c15673833d15b3a",
+    "url": "/static/js/main.4330d558.chunk.js"
+  },
+  {
+    "revision": "c6571f3d5199907c1210",
+    "url": "/static/js/runtime-main.3664d234.js"
+  },
+  {
+    "revision": "3733aae3c3cdad32b3a2bf5978811be8",
+    "url": "/static/media/Corner Logo.3733aae3.svg"
+  }
+]);

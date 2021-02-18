59407acec25f160f0ed9dffdf68e68e9952189c5
James Darby <56593075+Darbjm@users.noreply.github.com>
2021-02-18 11:54:30 +0300

    fixed like bug

---
+++ frontend/build/precache-manifest.95e67cfdfc3c422c2207875796740f17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1da9b3c01847a8b835b94ceb907b047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "439c1c7ddbfd5bf26b5c",
+    "url": "/static/css/main.872938e4.chunk.css"
+  },
+  {
+    "revision": "cfd22c52642e5ba0897d",
+    "url": "/static/js/2.54b8525c.chunk.js"
+  },
+  {
+    "revision": "bd7b26c0c34ce8f026397d952561bcd4",
+    "url": "/static/js/2.54b8525c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439c1c7ddbfd5bf26b5c",
+    "url": "/static/js/main.22fe8bfe.chunk.js"
+  },
+  {
+    "revision": "c6571f3d5199907c1210",
+    "url": "/static/js/runtime-main.3664d234.js"
+  },
+  {
+    "revision": "3733aae3c3cdad32b3a2bf5978811be8",
+    "url": "/static/media/Corner Logo.3733aae3.svg"
+  }
+]);

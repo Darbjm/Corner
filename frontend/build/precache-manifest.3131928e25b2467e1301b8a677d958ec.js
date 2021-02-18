60e421df46aa0a38a1962a0b55aabc0709df955a
James Darby <56593075+Darbjm@users.noreply.github.com>
2021-02-18 10:28:16 +0300

    built app

---
+++ frontend/build/precache-manifest.3131928e25b2467e1301b8a677d958ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd98d8c044a3efa382d9e6aec94cedbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f17fad75731d228d588e",
+    "url": "/static/css/main.872938e4.chunk.css"
+  },
+  {
+    "revision": "ebc3b8a2289a802918dd",
+    "url": "/static/js/2.8f40e026.chunk.js"
+  },
+  {
+    "revision": "bd7b26c0c34ce8f026397d952561bcd4",
+    "url": "/static/js/2.8f40e026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17fad75731d228d588e",
+    "url": "/static/js/main.cb8375e6.chunk.js"
+  },
+  {
+    "revision": "c6571f3d5199907c1210",
+    "url": "/static/js/runtime-main.3664d234.js"
+  },
+  {
+    "revision": "3733aae3c3cdad32b3a2bf5978811be8",
+    "url": "/static/media/Corner Logo.3733aae3.svg"
+  }
+]);
